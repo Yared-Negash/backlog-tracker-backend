e2bd8e822c30538b3b82543be9202ec884e2e0e1
Yared <yarednegash3115@gmail.com>
2021-03-02 00:25:42 -0500

    email,pass,and date into db

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const https = require('https');
 const app = express();
-
 const origin = process.env.ORIGIN || 'http://localhost:3001'
-console.log(`origin: ${origin}`);
-app.use(cors({
-    origin: origin
-}));
-
-
 const port = process.env.PORT || 3000;
 const baseOMDBLink = "https://www.omdbapi.com/?apikey=" + process.env.OMDB_KEY;
 
-console.log(baseOMDBLink);
+//app setup with cors to get requets from front end and various req.body inits
+app.use(cors({origin: origin}));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+console.log(`cors origin (Front-end): ${origin}`);
+console.log(`OMDB URL: ${baseOMDBLink}`);
+
+//mongoDB connection with USER schema
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+const userSchema = {emailAddress: String, userPassword: String, dateAdded: Date }
+const User = mongoose.model('User', userSchema);
 
 //home page
 app.get('/', (req, res) => {
@@ -39,7 +42,7 @@ app.get('/findLog', (req, res) => {
             body += data;
         })
         response.on('end', () => {
-            const logJSON = JSON.parse(body);        
+            const logJSON = JSON.parse(body);
             const logArray = logJSON.Search;
             if(!logArray){
                 console.log(`${logTitle} was not found`);
@@ -48,7 +51,7 @@ app.get('/findLog', (req, res) => {
             }
             let updatedLogArray = [];
             console.log(logJSON);
-            
+
             logArray.map(element =>{
                 updatedLogArray.push({ logTitle: element.Title, logPlot: "test", logReleaseDate: element.Year, logPoster: element.Poster });
             })
@@ -61,11 +64,18 @@ app.get('/findLog', (req, res) => {
 
     request.end();
 });
-app.post("/login",bodyParser.json(), (req, res) => {
+app.post("/login", (req, res) => {
     const emailAddress = req.body.emailAddress
     const userPassword = req.body.userPassword
+    const dateAdded = Date.now();
     console.log(JSON.stringify(req.body));
     console.log(`email ${emailAddress} and password ${userPassword}`);
+    const newUser = new User({ emailAddress: emailAddress, userPassword: userPassword,dateAdded:dateAdded });
+    newUser.save()
+        .then(() => {
+            console.log(`${emailAddress} was saved into the db`);
+        })
+    userPassword.save
     res.send("sending back from midware");
 });
 app.listen(port, () => {
