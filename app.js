60a5af565d2888304df96ead4b2703b56458aec2
Yared <yarednegash3115@gmail.com>
2021-03-22 01:06:48 -0400

    passport js first attempt (doesn't really work)

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = parseInt(process.env.SALT_ROUNDS); //converts string .env to int
 const cors = require('cors');
 const https = require('https');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose'); //note passport-local is already used as a dependecy for this
+
 const app = express();
 const origin = process.env.ORIGIN || 'http://localhost:3001'
 const port = process.env.PORT || 3000;
@@ -19,15 +21,29 @@ app.use(bodyParser.json());
 console.log(`cors origin (Front-end): ${origin}`);
 console.log(`OMDB URL: ${baseOMDBLink}`);
 
+app.use(session({
+    secret: "thisisatempstringshouldbeinenv",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 //mongoDB connection with USER schema
 mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema({
     emailAddress: String,
     userPassword: String,
     dateAdded: Date
 });
-
+userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //home page
 app.get('/', (req, res) => {
@@ -71,56 +87,62 @@ app.get('/findLog', (req, res) => {
 
     request.end();
 });
+
+function checkAuthenticated() {
+    console.log(`inside`);
+    if (req.isAuthenticated()) {
+        console.log(`user is authenticated`);
+        res.send(` successfully registered into the db`);
+    }
+    else {
+        console.log("user not found, redirecting to login");
+        res.redirect("/Login")
+    }
+};
 app.post("/register", (req, res) => {
     const emailAddress = req.body.emailAddress
     const userPassword = req.body.userPassword;
     const dateAdded = Date.now();
     console.log(`Creating account for ${emailAddress}`);
-    bcrypt.hash(userPassword, saltRounds, function (err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({ emailAddress: emailAddress, userPassword: hash, dateAdded: dateAdded });
-        newUser.save()
-            .then(() => {
-                console.log(`${emailAddress} was saved into the db`);
-                res.send(`${emailAddress} was successfully registered into the db`);
-            })
-            .catch((err) => {
-                console.log(`Error registering ${emailAddress} into the db: ${err}`);
-                res.send(`Error registering ${emailAddress} into the db: ${err}`);
-            })
+    User.register({ username: emailAddress, dateAdded: dateAdded }, userPassword, (err, user) => {
+        if (err) {
+            console.log(`Error registering ${emailAddress} into the db: ${err}`);
+            res.send(`Error registering ${emailAddress} into the db: ${err}`);
+        }
+        else {
+            passport.authenticate('local'),null,
+                function (req, res) {
+                    // If this function gets called, authentication was successful.
+                    // `req.user` contains the authenticated user.
+                    console.log(`inside`);
+                    if (req.isAuthenticated()) {
+                        console.log(`user is authenticated`);
+                        res.send(` successfully registered into the db`);
+                    }
+                    else {
+                        console.log("user not found, redirecting to login");
+                        res.redirect("/Login")
+                    }
+                }
+        }
     });
 });
 app.post("/login", (req, res) => {
-    const emailAddress = req.body.emailAddress
+    const emailAddress = req.body.emailAddress;
     const userPassword = req.body.userPassword;
-    const dateAdded = Date.now();
-    console.log(`Logging in ${emailAddress}`);
-    User.findOne({ emailAddress: emailAddress }, (err, foundUser) => {
-        if (err) {
-            console.log('ERROR HAPPEND: ' + err);
-            res.send(`Error occured while finding login credentials for ${emailAddress}`);
-        }
-        else {
-            let message = "";
-            if (foundUser) {
-                bcrypt.compare(userPassword,foundUser.userPassword, function (err, result) {
-                    // result == true
-                    if (result)
-                        message = `Found ${emailAddress}`;
-                    else
-                        message = `Incorrect password for ${emailAddress}`;
-                    console.log(message);
-                    res.send(message);
-                });
-            }
-            else{
-                message = `Could not find ${emailAddress} in the system`;
-                console.log(`Could not find ${emailAddress} in the system`);
-                res.send(message);
-            }
-        }
-    })
-});
+    console.log("test");
+    const user = new User({
+        username: emailAddress,
+        password: userPassword
+    });
+
+    passport.authenticate("local", (req, res, () => {
+        // If this function gets called, authentication was successful.
+        // `req.user` contains the authenticated user.
+        res.send(`${emailAddress} was found in the system`);
+    }));
+})
+
 app.listen(port, () => {
     if (origin.includes("localhost"))
         console.log(`Lumberjacks are awaiting your orders at http://localhost:${port}`)
