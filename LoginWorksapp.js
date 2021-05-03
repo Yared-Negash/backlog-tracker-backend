f892017429a34e558ab6854114e93e208cc30bdd
Yared <yarednegash3115@gmail.com>
2021-05-02 22:26:12 -0400

    first working version of login

--- LoginWorksapp.js
+++ LoginWorksapp.js
@@ -12,6 +12,8 @@ const passwordUtils = require('./utility/passwordUtil');
 const genPassword = passwordUtils.genPassword;
 const MongoStore = require('connect-mongo');
 
+
+
 const app = express();
 const origin = process.env.ORIGIN || 'http://localhost:3001'
 const port = process.env.PORT || 3000;
@@ -20,10 +22,73 @@ const baseOMDBLink = "https://www.omdbapi.com/?apikey=" + process.env.OMDB_KEY;
 //app setup with cors to get requets from front end and various req.body inits
 app.use(cors({ origin: origin }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 console.log(`cors origin (Front-end): ${origin}`);
 console.log(`OMDB URL: ${baseOMDBLink}`);
+
+/* //mongoDB connection with USER schema
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useCreateIndex', true);
+const userSchema = new mongoose.Schema({
+    username: String,
+    hash: String,
+    salt: String
+});
+userSchema.plugin(passportLocalMongoose);
+const User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy()); */
+
+/**
+ * This function is called when the `passport.authenticate()` method is called.
+ * 
+ * If a user is found an validated, a callback is called (`cb(null, user)`) with the user
+ * object.  The user object is then serialized with `passport.serializeUser()` and added to the 
+ * `req.session.passport` object. 
+ */
+/* passport.use(new LocalStrategy(
+    function (username, password, cb) {
+        User.findOne({ username: username })
+            .then((user) => {
+
+                if (!user) { return cb(null, false) }
+
+                // Function defined at bottom of app.js
+                const isValid = validPassword(password, user.hash, user.salt);
+
+                if (isValid) {
+                    return cb(null, user);
+                } else {
+                    return cb(null, false);
+                }
+            })
+            .catch((err) => {
+                cb(err);
+            });
+    })); */
+/**
+ * This function is used in conjunction with the `passport.authenticate()` method.  See comments in
+ * `passport.use()` above ^^ for explanation
+ */
+/* passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+}); */
+
+/**
+ * This function is used in conjunction with the `app.use(passport.session())` middleware defined below.
+ * Scroll down and read the comments in the PASSPORT AUTHENTICATION section to learn how this works.
+ * 
+ * In summary, this method is "set" on the passport object and is passed the user ID stored in the `req.session.passport`
+ * object later on.
+ */
+/* passport.deserializeUser(function (id, cb) {
+    User.findById(id, function (err, user) {
+        if (err) { return cb(err); }
+        cb(null, user);
+    });
+}); */
+
 
 //configures session data
 app.use(session({
@@ -35,6 +100,9 @@ app.use(session({
     },
     store: MongoStore.create({ mongoUrl: process.env.SESSION_STORE })
 }))
+
+
+
 /**
  * Connect to MongoDB Server using the connection string in the `.env` file.  To implement this, place the following
  * string into the `.env` file
@@ -42,27 +110,27 @@ app.use(session({
  * DB_STRING=mongodb://<user>:<password>@localhost:27017/database_name
  */
 
- const conn = process.env.DB_STRING;
+const conn = process.env.DB_STRING;
 
- const connection = mongoose.createConnection(conn, {
-     useNewUrlParser: true,
-     useUnifiedTopology: true
- });
- 
- 
- connection.on('connecting', () => {
-     console.log('connected');
- });
- 
- // Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
- const UserSchema = new mongoose.Schema({
-     username: String,
-     hash: String,
-     salt: String
- });
- 
- 
- const User = connection.model('User', UserSchema);
+const connection = mongoose.createConnection(conn, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+connection.on('connecting', () => {
+    console.log('connected');
+});
+
+// Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
+const UserSchema = new mongoose.Schema({
+    username: String,
+    hash: String,
+    salt: String
+});
+
+
+const User = connection.model('User', UserSchema);
 
 
 /**
@@ -72,7 +140,7 @@ app.use(session({
  * object.  The user object is then serialized with `passport.serializeUser()` and added to the 
  * `req.session.passport` object. 
  */
- passport.use(new LocalStrategy(
+passport.use(new LocalStrategy(
     { // or whatever you want to use
         usernameField: 'emailAddress',    // define the parameter in req.body that passport can use as username and password
         passwordField: 'userPassword'
@@ -135,6 +203,7 @@ app.use(passport.initialize());
 app.use(passport.session()); 
 
 
+
 //home page
 app.get('/', (req, res) => {
     console.log("Here is the api key" + process.env.OMDB_KEY);
@@ -178,6 +247,17 @@ app.get('/findLog', (req, res) => {
     request.end();
 });
 
+function checkAuthenticated() {
+    console.log(`inside`);
+    if (req.isAuthenticated()) {
+        console.log(`user is authenticated`);
+        res.send(` successfully registered into the db`);
+    }
+    else {
+        console.log("user not found, redirecting to login");
+        res.redirect("/Login")
+    }
+};
 app.post("/register", (req, res) => {
     const emailAddress = req.body.emailAddress
     const userPassword = req.body.userPassword;
@@ -191,19 +271,18 @@ app.post("/register", (req, res) => {
 
     const newUser = new User({
         username: emailAddress,
-        hash : hash,
-        salt : salt
+        hash: hash,
+        salt: salt
     });
 
     //save new user into database with generated hash and salt
     newUser.save()
-        .then((user) =>{
-            console.log(`${user} successfuly registerd`);
-            res.send({"registerStatus":true})
+        .then((user) => {
+            res.send({ "registerStatus": true })
         })
-        .catch((err) =>{
+        .catch((err) => {
             console.log(`error occured ${err}`);
-            res.send({"registerStatus":false})
+            res.send({ "registerStatus": false })
         })
 });
 // Since we are using the passport.authenticate() method, we should be redirected no matter what 
